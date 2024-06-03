8d3e71b4d2b0112c9d68ed22e822e36b4e4220b9
T0M14 <wevertonsilva0614@gmail.com>
2024-06-03 00:58:32 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,31 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const yesButton = document.getElementById('yes');
+    const noButton = document.getElementById('no');
+
+    yesButton.addEventListener('click', () => {
+        window.location.href = 'https://youtu.be/7yW_uJOpxww?si=yLpgSs8TvDQo38kB';
+    });
+
+    noButton.addEventListener('mouseover', () => {
+        const randomX = Math.floor(Math.random() * window.innerWidth);
+        const randomY = Math.floor(Math.random() * window.innerHeight);
+
+        noButton.style.position = 'absolute';
+        noButton.style.left = `${randomX}px`;
+        noButton.style.top = `${randomY}px`;
+    });
+
+    function createHeart() {
+        const heart = document.createElement('div');
+        heart.classList.add('heart');
+        heart.style.left = `${Math.random() * 100}vw`;
+        heart.style.animationDuration = `${Math.random() * 2 + 3}s`;
+        document.getElementById('hearts-container').appendChild(heart);
+
+        setTimeout(() => {
+            heart.remove();
+        }, 5000);
+    }
+
+    setInterval(createHeart, 300);
+});
